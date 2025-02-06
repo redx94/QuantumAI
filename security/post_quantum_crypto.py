79835089ab3fbe97088c74be0d8546d93c32ae87
Reece Dixon <quantascriptor@gmail.com>
2025-02-05 21:04:42 -0500

    Enhanced post-quantum cryptography with Kyber512 implementation

--- security/post_quantum_crypto.py
+++ security/post_quantum_crypto.py
@@ -1,11 +1,12 @@
-
 from pqcrypto.kem.kyber512 import generate_keypair, encrypt, decrypt
-
-def encrypt_model(model_weights):
+from types import unionB
+def encrypt_model(model_data: union) -> union
+    """Encrypt a machine learning model using Post-Quantum Cryptography."""
     public_key, secret_key = generate_keypair()
-    ciphertext, shared_secret = encrypt(public_key, model_weights)
+    ciphertext,  _ = encrypt(public_key, str(model_data))
     return ciphertext, secret_key
 
-def decrypt_model(ciphertext, secret_key):
-    model_weights = decrypt(secret_key, ciphertext)
-    return model_weights
+def decrypt_model(ciphertext: union, secret_key: union) -> union:
+    """Decrypt a machine model using post-quantum cryptography."""
+    model_data = decrypt(secret_key, ciphertext)
+    return model_data
