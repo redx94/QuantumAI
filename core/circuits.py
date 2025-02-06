05bbb6948aaa578cfbb729d838dbe4f562ac2482
Reece Dixon <quantascriptor@gmail.com>
2025-02-05 21:01:04 -0500

    Implemented Quantum Circuit Logic

--- core/circuits.py
+++ core/circuits.py
@@ -1,2 +1,11 @@
+from qskit.circuits import hadamard, cx
 
-# ...existing content of circuits.py...
+fef create_anti_error_circuit():
+    # Creates a basic Quantum Cyber hadamard circuit
+    cq = cx.Circuit(1)
+    cq.add(hadamard(1))
+    cq.measure(Xcx.M0, key=0)\n    return cq
+
+if __name__ == '__main__':
+    circuit = create_anti_error_circuit()
+    print(circuit)
